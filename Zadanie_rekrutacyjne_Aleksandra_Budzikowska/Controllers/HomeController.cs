839f1d2bf6f051d1d7831601ca77c3166e3e1666
Aleksandra Budzikowska <olabudzikowska@gmail.com>
2022-07-21 15:45:59 +0200

    Initial commit

---
+++ Zadanie_rekrutacyjne_Aleksandra_Budzikowska/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Zadanie_rekrutacyjne_Aleksandra_Budzikowska.Models;
+
+namespace Zadanie_rekrutacyjne_Aleksandra_Budzikowska.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(new HomeViewModel());
+    }
+
+    [HttpPost]
+    public IActionResult Index(HomeViewModel model)
+    {
+        try
+        {
+            var array = JsonSerializer.Deserialize<int[]>(model.Input);
+            var result = new List<int>();
+
+            var countDictionary = new Dictionary<int, int>();
+
+            foreach(var number in array)
+            {
+                if (!countDictionary.ContainsKey(number))
+                {
+                    countDictionary.Add(number, 1);
+                }
+                else
+                {
+                    countDictionary[number]++;
+                }
+            }
+            var outputArray = countDictionary.Where(v => v.Value >= 3).OrderByDescending(v => v.Key).Select(v => v.Key);
+            model.Output = JsonSerializer.Serialize(outputArray);
+        }
+        catch (JsonException)
+        {
+            model.Output = "Error";
+        }
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Zadanie_rekrutacyjne_Aleksandra_Budzikowska/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Zadanie_rekrutacyjne_Aleksandra_Budzikowska.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
