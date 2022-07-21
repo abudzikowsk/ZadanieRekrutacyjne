5cfab3f4b8157963a77d81916059ff4f07038ac5
Aleksandra Budzikowska <olabudzikowska@gmail.com>
2022-07-21 15:46:27 +0200

    Add missing file

---
+++ Zadanie_rekrutacyjne_Aleksandra_Budzikowska/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Zadanie_rekrutacyjne_Aleksandra_Budzikowska.Models
+{
+    public class HomeViewModel
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
+
